fb82a7b5c9ba728bbd6ba9bebc078099b18512d0
channah345 <channah@cdrme.co.uk>
2026-05-20 21:59:48 +0100

    Update __init__.py

--- cdr_core/__init__.py
+++ cdr_core/__init__.py
@@ -1,9 +1,27 @@
 from .dates import now_log_time, graph_datetime_now, get_today_iso
 from .config import CDRConfig
+from .statuses import (
+    AWAITING_DEPLOYMENT_STATUS,
+    LEGACY_AWAITING_DEPLOYMENT_STATUS,
+    ASSIGNED_STATUS,
+    TRAVELLING_STATUS,
+    ON_SITE_STATUS,
+    COMPLETED_STATUS,
+    DAY_ACTIVE_STATUS,
+    DAY_CLOSED_STATUS,
+)
 
 __all__ = [
     "now_log_time",
     "graph_datetime_now",
     "get_today_iso",
     "CDRConfig",
+    "AWAITING_DEPLOYMENT_STATUS",
+    "LEGACY_AWAITING_DEPLOYMENT_STATUS",
+    "ASSIGNED_STATUS",
+    "TRAVELLING_STATUS",
+    "ON_SITE_STATUS",
+    "COMPLETED_STATUS",
+    "DAY_ACTIVE_STATUS",
+    "DAY_CLOSED_STATUS",
 ]
